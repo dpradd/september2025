47cfdc1d03b97afaa2b1fb28513745202dac0079
Darius Pradzevicius <darius.pradzevicius@macaw.nl>
2025-10-08 23:09:33 +0300

    test

--- .github/terraform/prod/main.tf
+++ .github/terraform/prod/main.tf
@@ -13,3 +13,36 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-demo-${var.env}"
   location = var.location
 }
+
+resource "azurerm_service_plan" "plan" {
+  name                = "ghactionsplan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+resource "azurerm_linux_web_app" "app" {
+  name                = "dprgawebapp${var.env}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  service_plan_id     = azurerm_service_plan.plan.id
+
+  site_config {
+    application_stack {
+      node_version = "18-lts"
+    }
+  }
+
+  app_settings = {
+    WEBSITE_RUN_FROM_PACKAGE = "1"
+  }
+}
+
+output "webapp_name" {
+  value = azurerm_linux_web_app.app.name
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
