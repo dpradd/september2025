b1a92e382b7670a783b864301a3ca5e82e4f3dfd
Darius Pradzevicius <darius.pradzevicius@macaw.nl>
2025-10-09 15:49:11 +0300

    test

--- .github/terraform/prod/main.tf
+++ .github/terraform/prod/main.tf
@@ -46,3 +46,7 @@ output "webapp_name" {
 output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
+
+output "web_app_url" {
+  value = azurerm_linux_web_app.app.default_site_hostname
+}
